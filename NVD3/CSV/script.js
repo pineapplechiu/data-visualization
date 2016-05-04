c2b1692fa6353a5e4697d5e1ca8944693f12b6e9
paulachiu <chiupaula@gmail.com>
2016-05-03 23:06:21 -0700

    read CSV files for use with NVD3

---
+++ NVD3/CSV/script.js
@@ -0,0 +1,35 @@
+d3.csv("sample.csv", function(error, data){
+  console.log(data)
+  // create an empty object that nv is expecting
+  var exampleData = [
+    {
+      key: "totals",
+      values: []
+    }
+  ];
+
+  // populate the empty object with your data
+  data.forEach(function (d){
+    d.value = +d.value
+    exampleData[0].values.push(d)
+  })       
+
+nv.addGraph(function() {
+  var chart = nv.models.discreteBarChart()
+      .x(function (d) { console.log(d); return d.label })
+      .y(function (d) { return d.value })
+      .staggerLabels(true)
+      .tooltips(false)
+      .showValues(true)
+
+  d3.select('#chart')
+      .datum(exampleData)
+      .attr("id", function (d) { console.log(d); })
+    .transition().duration(500)
+        .call(chart);
+
+  nv.utils.windowResize(chart.update);
+  return chart;
+});
+
+});
